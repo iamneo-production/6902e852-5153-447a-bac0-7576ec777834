cd4d3048451ea20e7fdf6fee6000a76725224a32
argha23 <arghadipbagchi12@gmail.com>
2022-03-24 00:55:10 +0530

    Backend for user

---
+++ springapp/src/main/java/com/examly/model/UserModel.java
@@ -0,0 +1,102 @@
+package com.examly.model;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "login")
+public class UserModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String useroradmin;
+    private String email;
+    private String username;
+    private String phonenumber;
+    private String password;
+    @Transient
+    private String confirmpassword;
+
+    @OneToMany(cascade = CascadeType.ALL, targetEntity = ProductModel.class)
+    private List<ProductModel> productModel = new ArrayList<>();
+
+    public UserModel() {
+
+    }
+
+    public UserModel(String username, String email) {
+        this.username = username;
+        this.email = email;
+    }
+
+    public List<ProductModel> getProductModel() {
+        return productModel;
+    }
+
+    public void ProductModel(List<ProductModel> productModel) {
+        this.productModel = productModel;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getConfirmpassword() {
+        return confirmpassword;
+    }
+
+    public void setConfirmpassword(String confirmpassword) {
+        this.confirmpassword = confirmpassword;
+    }
+
+    public String getUseroradmin() {
+        return useroradmin;
+    }
+
+    public void setUseroradmin(String useroradmin) {
+        this.useroradmin = useroradmin;
+    }
+
+    public String getPhonenumber() {
+        return phonenumber;
+    }
+
+    public void setPhonenumber(String phonenumber) {
+        this.phonenumber = phonenumber;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getConfrmpassword() {
+        return confirmpassword;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+
+}
