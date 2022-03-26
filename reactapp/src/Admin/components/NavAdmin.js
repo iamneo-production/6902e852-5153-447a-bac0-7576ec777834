b80f96fd4e48ec691565e1dddccd15d807a5fc89
argha23 <arghadipbagchi12@gmail.com>
2022-03-26 19:57:22 +0530

    Admin done

--- reactapp/src/Admin/components/NavAdmin.js
+++ reactapp/src/Admin/components/NavAdmin.js
@@ -12,179 +12,144 @@ import Button from '@mui/material/Button';
 import Tooltip from '@mui/material/Tooltip';
 import MenuItem from '@mui/material/MenuItem';
 
-import { Link } from 'react-router-dom'
+import {Link} from 'react-router-dom'
 
 const pages = ['AddCenter', 'CenterProfile'];
 const settings = ['Logout'];
 
 const NavAdmin = () => {
-    const [anchorElNav, setAnchorElNav] = React.useState(null);
-    const [anchorElUser, setAnchorElUser] = React.useState(null);
+  const [anchorElNav, setAnchorElNav] = React.useState(null);
+  const [anchorElUser, setAnchorElUser] = React.useState(null);
 
-    const handleOpenNavMenu = (event) => {
-        setAnchorElNav(event.currentTarget);
-    };
-    const handleOpenUserMenu = (event) => {
-        setAnchorElUser(event.currentTarget);
-    };
+  const handleOpenNavMenu = (event) => {
+    setAnchorElNav(event.currentTarget);
+  };
+  const handleOpenUserMenu = (event) => {
+    setAnchorElUser(event.currentTarget);
+  };
 
-    const handleCloseNavMenu = () => {
-        setAnchorElNav(null);
-    };
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
 
-    const handleCloseUserMenu = () => {
-        setAnchorElUser(null);
-    };
+  const handleCloseUserMenu = () => {
+    setAnchorElUser(null);
+  };
 
-    return ( <
-        div >
-        <
-        AppBar position = "static" >
-        <
-        Container maxWidth = "xl" >
-        <
-        Toolbar disableGutters >
-        <
-        Typography variant = "h6"
-        noWrap component = "div"
-        sx = {
-            { mr: 2, display: { xs: 'none', md: 'flex' } } } >
-        Impresser <
-        /Typography>
+  return (
+    <div>
+    <AppBar position="static">
+      <Container maxWidth="xl">
+        <Toolbar disableGutters>
+          <Typography
+            variant="h6"
+            noWrap
+            component="div"
+            sx={{ mr: 2, display: { xs: 'none', md: 'flex' } }}
+          >
+            Impresser
+          </Typography>
 
-        <
-        Box sx = {
-            { flexGrow: 1, display: { xs: 'flex', md: 'none' } } } >
-        <
-        IconButton size = "large"
-        aria - label = "account of current user"
-        aria - controls = "menu-appbar"
-        aria - haspopup = "true"
-        onClick = { handleOpenNavMenu }
-        color = "inherit" >
-        <
-        MenuIcon / >
-        <
-        /IconButton> <
-        Menu id = "menu-appbar"
-        anchorEl = { anchorElNav }
-        anchorOrigin = {
-            {
+          <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+            <IconButton
+              size="large"
+              aria-label="account of current user"
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+              onClick={handleOpenNavMenu}
+              color="inherit"
+            >
+              <MenuIcon />
+            </IconButton>
+            <Menu
+              id="menu-appbar"
+              anchorEl={anchorElNav}
+              anchorOrigin={{
                 vertical: 'bottom',
                 horizontal: 'left',
-            }
-        }
-        keepMounted transformOrigin = {
-            {
+              }}
+              keepMounted
+              transformOrigin={{
                 vertical: 'top',
                 horizontal: 'left',
-            }
-        }
-        open = { Boolean(anchorElNav) }
-        onClose = { handleCloseNavMenu }
-        sx = {
-            {
+              }}
+              open={Boolean(anchorElNav)}
+              onClose={handleCloseNavMenu}
+              sx={{
                 display: { xs: 'block', md: 'none' },
-            }
-        } >
-        {
-            pages.map((page) => ( <
-                MenuItem key = { page }
-                onClick = { handleCloseNavMenu } >
-                <
-                Typography textAlign = "center" >
-                <
-                Link style = {
-                    { textDecoration: "none", color: "white" } }
-                to = { `/${page}` } > { page } <
-                /Link>
+              }}
+            >
+              {pages.map((page) => (
+                <MenuItem key={page} onClick={handleCloseNavMenu}>
+                  <Typography textAlign="center">
+                      <Link style={{textDecoration:"none",color:"white"}} to={`/${page}`}>
+                        {page}
+                      </Link>
+                     
+                  </Typography>
+                </MenuItem>
+              ))}
+            </Menu>
+          </Box>
+          <Typography
+            variant="h6"
+            noWrap
+            component="div"
+            sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}
+          >
+            Impresser
+          </Typography>
+          <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+            {pages.map((page) => (
+              <Button
+                key={page}
+                onClick={handleCloseNavMenu}
+                sx={{ my: 2, color: 'white', display: 'block' }}
+              >
+               <Link style={{textDecoration:"none",color:"white"}} to={`/${page}`}>
+                        {page}
+                      </Link>
+              </Button>
+            ))}
+          </Box>
 
-                <
-                /Typography> <
-                /MenuItem>
-            ))
-        } <
-        /Menu> <
-        /Box> <
-        Typography variant = "h6"
-        noWrap component = "div"
-        sx = {
-            { flexGrow: 1, display: { xs: 'flex', md: 'none' } } } >
-        Impresser <
-        /Typography> <
-        Box sx = {
-            { flexGrow: 1, display: { xs: 'none', md: 'flex' } } } > {
-            pages.map((page) => ( <
-                Button key = { page }
-                onClick = { handleCloseNavMenu }
-                sx = {
-                    { my: 2, color: 'white', display: 'block' } } >
-                <
-                Link style = {
-                    { textDecoration: "none", color: "white" } }
-                to = { `/${page}` } > { page } <
-                /Link> <
-                /Button>
-            ))
-        } <
-        /Box>
-
-        <
-        Box sx = {
-            { flexGrow: 0 } } >
-        <
-        Tooltip title = "Logout" >
-        <
-        IconButton onClick = { handleOpenUserMenu }
-        sx = {
-            { p: 0 } } >
-        <
-        Avatar alt = "A"
-        src = "/static/images/avatar/2.jpg" / >
-        <
-        /IconButton> <
-        /Tooltip> <
-        Menu sx = {
-            { mt: '45px' } }
-        id = "menu-appbar"
-        anchorEl = { anchorElUser }
-        anchorOrigin = {
-            {
+          <Box sx={{ flexGrow: 0 }}>
+            <Tooltip title="Logout">
+              <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                <Avatar alt="A" src="/static/images/avatar/2.jpg" />
+              </IconButton>
+            </Tooltip>
+            <Menu
+              sx={{ mt: '45px' }}
+              id="menu-appbar"
+              anchorEl={anchorElUser}
+              anchorOrigin={{
                 vertical: 'top',
                 horizontal: 'right',
-            }
-        }
-        keepMounted transformOrigin = {
-            {
+              }}
+              keepMounted
+              transformOrigin={{
                 vertical: 'top',
                 horizontal: 'right',
-            }
-        }
-        open = { Boolean(anchorElUser) }
-        onClose = { handleCloseUserMenu } >
-        {
-            settings.map((setting) => ( <
-                Link to = "/Login"
-                style = {
-                    { textDecoration: "none", color: "black" } } >
-                <
-                MenuItem key = { setting }
-                onClick = { handleCloseUserMenu } >
-                <
-                Typography textAlign = "center" > { setting } < /Typography> <
-                /MenuItem> <
-                /Link>
-            ))
-        } <
-        /Menu> <
-        /Box> <
-        /Toolbar> <
-        /Container> <
-        /AppBar> <
-        div >
-        <
-        /div> <
-        /div>
-    );
+              }}
+              open={Boolean(anchorElUser)}
+              onClose={handleCloseUserMenu}
+            >
+              {settings.map((setting) => (
+                <Link to="/Login" style={{textDecoration:"none",color:"black"}}>
+                <MenuItem key={setting} onClick={handleCloseUserMenu}>
+                  <Typography textAlign="center">{setting}</Typography>
+                </MenuItem>
+                </Link>
+              ))}
+            </Menu>
+          </Box>
+        </Toolbar>
+      </Container>
+    </AppBar>
+    <div>
+    </div>
+  </div>
+  );
 };
 export default NavAdmin;
--- reactapp/src/Admin/components/Update.js
+++ reactapp/src/Admin/components/Update.js
@@ -11,130 +11,104 @@ function Update() {
     const location = useLocation();
     const [state, setState] = useState(location.state);
     const handleUpdate = (e) => {
-        e.preventDefault();
-        console.log("submitted");
-        axios
-            .put(`http://localhost:8080/update/${state.id}`, state)
-            .then((data) => {
-                navigate("/CenterProfile");
-                console.log(data);
-            })
-            .catch((err) => {
-                console.log(err);
-            });
+      e.preventDefault();
+      console.log("submitted");
+      axios
+        .put(`http://localhost:8080/update/${state.id}`, state)
+        .then((data) => {
+          navigate("/CenterProfile");
+          console.log(data);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
     };
     const changeState = (value, name) => {
-        setState({...state, [name]: value });
+      setState({ ...state, [name]: value });
     };
     console.log(navigate);
     const {
-        serviceCenterName,
-        serviceCenterPhone,
-        serviceCenterAddress,
-        serviceCenterImage,
-        serviceCenterEmail,
+      serviceCenterName,
+      serviceCenterPhone,
+      serviceCenterAddress,
+      serviceCenterImage,
+      serviceCenterEmail,
     } = state;
-    return ( <
-        div className = "Updatemain " >
-        <
-        Form className = "formCard" >
-        <
-        h2 > Update Service < /h2> <
-        Form.Group className = "mb-3"
-        controlId = "exampleForm.ControlInput1" >
-        <
-        Form.Label > Name < /Form.Label> <
-        br > < /br> <
-        Form.Control name = "serviceCenterName"
-        value = { serviceCenterName }
-        onChange = {
-            (e) => changeState(e.target.value, e.target.name) }
-        type = "text"
-        placeholder = "Enter Name" /
-        >
-        <
-        /Form.Group>
-
-        <
-        Form.Group className = "mb-3"
-        controlId = "exampleForm.ControlInput1" >
-        <
-        Form.Label > Email < /Form.Label> <
-        br > < /br> <
-        Form.Control name = "serviceCenterEmail"
-        type = "text"
-        value = { serviceCenterEmail }
-        onChange = {
-            (e) => changeState(e.target.value, e.target.name) }
-        placeholder = "Enter Email" /
-        >
-        <
-        /Form.Group>
-
-        <
-        Form.Group className = "mb-3"
-        controlId = "exampleForm.ControlInput1" >
-        <
-        Form.Label > Phone < /Form.Label> <
-        br > < /br> <
-        Form.Control name = "serviceCenterPhone"
-        type = "text"
-        value = { serviceCenterPhone }
-        onChange = {
-            (e) => changeState(e.target.value, e.target.name) }
-        placeholder = "Enter Phone" /
-        >
-        <
-        /Form.Group>
-
-        <
-        Form.Group className = "mb-3"
-        controlId = "exampleForm.ControlInput1" >
-        <
-        Form.Label > Address < /Form.Label> <
-        br > < /br> <
-        Form.Control name = "serviceCenterAddress"
-        type = "text"
-        value = { serviceCenterAddress }
-        onChange = {
-            (e) => changeState(e.target.value, e.target.name) }
-        placeholder = "Enter Address" /
-        >
-        <
-        /Form.Group>
-
-        <
-        Form.Group className = "mb-3"
-        controlId = "exampleForm.ControlInput1" >
-        <
-        Form.Label > Img Url < /Form.Label> <
-        br > < /br> <
-        Form.Control name = "serviceCenterImage"
-        type = "text"
-        value = { serviceCenterImage }
-        onChange = {
-            (e) => changeState(e.target.value, e.target.name) }
-        placeholder = "Enter ImageUrl" /
-        >
-        <
-        /Form.Group>
-
-        <
-        Button variant = "danger"
-        type = "submit"
-        onClick = { handleUpdate } >
-        Submit <
-        /Button> <
-        Link to = "/CenterProfile" >
-        <
-        Button variant = "danger"
-        className = "deleteCard" >
-        Back <
-        /Button> <
-        /Link> <
-        /Form> <
-        /div>
+    return (
+      <div className="Updatemain ">
+        <Form className="formCard">
+          <h2>Update Service</h2>
+          <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+            <Form.Label>Name</Form.Label>
+            <br></br>
+            <Form.Control
+              name="serviceCenterName"
+              value={serviceCenterName}
+              onChange={(e) => changeState(e.target.value, e.target.name)}
+              type="text"
+              placeholder="Enter Name"
+            />
+          </Form.Group>
+  
+          <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+            <Form.Label>Email</Form.Label>
+            <br></br>
+            <Form.Control
+              name="serviceCenterEmail"
+              type="text"
+              value={serviceCenterEmail}
+              onChange={(e) => changeState(e.target.value, e.target.name)}
+              placeholder="Enter Email"
+            />
+          </Form.Group>
+  
+          <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+            <Form.Label>Phone</Form.Label>
+            <br></br>
+            <Form.Control
+              name="serviceCenterPhone"
+              type="text"
+              value={serviceCenterPhone}
+              onChange={(e) => changeState(e.target.value, e.target.name)}
+              placeholder="Enter Phone"
+            />
+          </Form.Group>
+  
+          <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+            <Form.Label>Address</Form.Label>
+            <br></br>
+            <Form.Control
+              name="serviceCenterAddress"
+              type="text"
+              value={serviceCenterAddress}
+              onChange={(e) => changeState(e.target.value, e.target.name)}
+              placeholder="Enter Address"
+            />
+          </Form.Group>
+  
+          <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+            <Form.Label>Img Url</Form.Label>
+            <br></br>
+            <Form.Control
+              name="serviceCenterImage"
+              type="text"
+              value={serviceCenterImage}
+              onChange={(e) => changeState(e.target.value, e.target.name)}
+              placeholder="Enter ImageUrl"
+            />
+          </Form.Group>
+  
+          <Button variant="danger" type="submit" onClick={handleUpdate}>
+            Submit
+          </Button>
+          <Link to="/CenterProfile">
+            <Button variant="danger" className="deleteCard">
+              Back
+            </Button>
+          </Link>
+        </Form>
+      </div>
     );
-}
+  }
 
 export default Update
