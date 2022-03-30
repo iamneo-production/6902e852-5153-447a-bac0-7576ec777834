0d9df881853f7cbf56a6cf98c12d18335c374a25
Madhumitha05072000 <madh18123.it@rmkec.ac.in>
2022-03-30 22:41:24 +0530

    Review and Review Repository

---
+++ springapp/src/main/java/com/examly/Repository/ReviewRepository.java
@@ -0,0 +1,9 @@
+package com.examly.Repository;
+
+import com.examly.model.Review;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ReviewRepository extends JpaRepository<Review, Long> {
+}
