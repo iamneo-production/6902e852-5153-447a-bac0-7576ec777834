6978711cb07d1b6fc697131d896c35111600d83b
Shruthi341 <alurishruthi341@gmail.com>
2022-03-30 21:47:22 +0530

    updated

--- reactapp/src/Fields/TextBar.jsx
+++ reactapp/src/Fields/TextBar.jsx
@@ -1,56 +1,18 @@
-import React from "react";
-import { ErrorMessage, useField } from "formik";
+import React from 'react';
+import { ErrorMessage, useField } from 'formik';
 
-function TextBar({ label, type, ...props }) {
+function TextBar ({ label, ...props }){
   const [field, meta] = useField(props);
-  switch (type) {
-    case "select":
-      return (
-        <div className="mb-2">
-          <label
-            htmlFor={field.name}
-            style={{
-              display: "flex",
-              justifyContent: "flex-start",
-              fontSize: 17,
-            }}
-          >
-            {label}
-          </label>
-          <select {...field} {...props} >
-            <option>USERORADMIN</option>
-            <option value="user">USER</option>
-            <option value="admin">ADMIN</option>
-          </select>
-          <ErrorMessage component="div" className="error" name={field.name} />
-        </div>
-      );
-    default:
-      return (
-        <div className="mb-2">
-          <label
-            htmlFor={field.name}
-            style={{
-              display: "flex",
-              justifyContent: "flex-start",
-              fontSize: 17,
-            }}
-          >
-            {label}
-          </label>
-          <input
-            type={type}
-            className={`form-control shadow-none ${
-              meta.touched && meta.error && "is-invalid"
-            }`}
-            {...field}
-            {...props}
-            autoComplete="off"
-          />
-          <ErrorMessage component="div" className="error" name={field.name} />
-        </div>
-      );
-  }
- 
+  return (
+    <div className="mb-2">
+      <label htmlFor={field.name} style={{display:"flex",justifyContent:'flex-start',fontSize:17}}>{label}</label>
+      <input
+        className={`form-control shadow-none ${meta.touched && meta.error && 'is-invalid'}`}
+        {...field} {...props}
+        autoComplete="off"
+      />
+      <ErrorMessage component="div" name={field.name} />
+    </div>
+  )
 }
 export default TextBar;
