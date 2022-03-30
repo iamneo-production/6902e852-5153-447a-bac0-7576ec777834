679a2013beeef5970a08c4186ccb1b8b228d6d95
Sridharan462 <gsridhar462@gmail.com>
2022-03-30 22:23:01 +0530

    security

---
+++ springapp/src/main/java/com/examly/Security/MyUserDetailsService.java
@@ -0,0 +1,26 @@
+package com.example.Security;
+
+import com.example.Repository.UserRepository;
+import com.example.model.UserModel;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+@Service
+public class MyUserDetailsService implements UserDetailsService {
+    @Autowired
+    private UserRepository authrepo;
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        UserModel user=authrepo.findByEmail(email);
+        if (user == null) {
+            throw new UsernameNotFoundException("User not found with username: " + email);
+        }
+        return new org.springframework.security.core.userdetails.User(user.getEmail(), user.getPassword(),
+                new ArrayList<>());
+
+    }
+}
