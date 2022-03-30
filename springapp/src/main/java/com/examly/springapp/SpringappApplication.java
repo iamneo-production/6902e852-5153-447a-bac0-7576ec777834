58030c31b01fb40e109004efda88aafbef40b484
haripriyau <haripriya.u2018ece@sece.ac.in>
2022-03-30 19:51:29 +0530

    Filed Added

--- springapp/src/main/java/com/examly/springapp/SpringappApplication.java
+++ springapp/src/main/java/com/examly/springapp/SpringappApplication.java
@@ -1,9 +1,14 @@
 package com.examly.springapp;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
+@Configuration
+@EnableAutoConfiguration
 public class SpringappApplication {
 
 	public static void main(String[] args) {
