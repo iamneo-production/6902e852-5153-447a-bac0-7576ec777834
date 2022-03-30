11c02bbbd5cba6191d0d3ee51321582a81269dbd
Jaghan-Vishwanathan <jaghana@virtusa.com>
2022-03-30 22:34:01 +0530

    User Details

---
+++ springapp/src/main/java/com/examly/Security/UserDetails.java
@@ -0,0 +1,21 @@
+package com.examly.Security;
+
+import org.springframework.security.core.GrantedAuthority;
+
+import java.io.Serializable;
+import java.util.Collection;
+
+public interface UserDetails extends Serializable {
+    Collection<? extends GrantedAuthority> getAuthorities();
+    String getPassword();
+    String getEmail();
+
+    boolean isAccountNonExpired();
+
+    boolean isAccountNonLocked();
+
+    boolean isCredentialsNonExpired();
+
+    boolean isEnabled();
+
+}
